927a6f447591aab189b70f517ed69f360d15ecc9
Ainoa Urquidi <148345452+ainoaur-uma@users.noreply.github.com>
2024-02-18 19:51:23 +0100

    guardar todo

--- api/routes/recetaRoutes.js
+++ api/routes/recetaRoutes.js
@@ -7,7 +7,7 @@ const recetaController = require('../controllers/recetaController');
 //Definición de rutas para la entidad receta:
 router.post('/', recetaController.create); // Crear una nueva receta
 router.get('/', recetaController.findAll); // Obtener todas las recetas
-router.get('/detalles', recetaController.getRecetasDetails); // Obtener todas las recetas con detalles extendidos
+router.get('/listadetallada', recetaController.getRecetasDetails); // Obtener todas las recetas con detalles extendidos
 router.get('/:idReceta', recetaController.findOne); // Obtener una receta específica por su ID
 router.get('/paciente/:nhc_paciente', recetaController.findByNhc); // Obtener recetas por NHC del paciente
 router.get('/medicamento/:idMedicamento', recetaController.findByMedicamentoId); // Obtener recetas por ID del medicamento
